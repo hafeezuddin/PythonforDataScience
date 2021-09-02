4aec96df62f2464d798c12a5c5b931dda3ca6d76
Shaik Khaja Hafeezuddin <hafeezskhbablu@gmail.com>
2021-09-01 23:12:56 -0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -8,11 +8,22 @@ print(type(a))
 b=3.4
 print(type(b))
 
-c="Hello World!"   #string can be represented in double quotes
-d='Hello, buddy!'  #string can be represented in single quotes
+c="Hello World!"   #string can be represented in double quotes.
+d='Hello, buddy!'  #string can be represented in single quotes.
 print(type(c))    #Variables C & D are complex DT.
 print(type(d))
 
 e=3+4j
 print(type(e))
 
+#Type Conversions
+a = float(a)
+print(a)
+print(type(a))
+
+b = int(a)
+print(type(b))
+print(b)
+
+c=float(a)
+print(type(c))
