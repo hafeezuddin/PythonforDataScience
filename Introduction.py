6ac8a86e801576b421034bd9d899c5060fe27d18
Shaik Khaja Hafeezuddin <hafeezskhbablu@gmail.com>
2021-09-01 23:08:24 -0700

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,18 @@
+#Comments in python are wrtten after #
+
+a = 3  #Variable a is assigned with int data type
+#a is an interger. Python automatically detects the data type of the variable.
+print(a)
+print(type(a))
+
+b=3.4
+print(type(b))
+
+c="Hello World!"   #string can be represented in double quotes
+d='Hello, buddy!'  #string can be represented in single quotes
+print(type(c))    #Variables C & D are complex DT.
+print(type(d))
+
+e=3+4j
+print(type(e))
+
