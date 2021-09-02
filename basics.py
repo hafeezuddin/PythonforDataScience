1dca4b99cecaa19ee2410fa8187705e41bfc0003
Hafeezuddin Shaik <hafeezskhbablu@gmail.com>
2021-09-02 13:02:29 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,7 @@
+a=input("Enter the Data:") #Taking input as string
+print(type(a))  #Typing data type of a
+a=int(a) #conversion of string into int
+print(type(a))
+
+b=int(input()) #conversion in same line of taking input
+print(type(b))
